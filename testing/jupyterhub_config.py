38ff9cb3000c495900bb993279bedffe8edc5c99
Tan Phan <ptdtan@gmail.com>
2021-10-01 11:11:27 -0400

    add testing config

--- testing/jupyterhub_config.py
+++ testing/jupyterhub_config.py
@@ -20,7 +20,10 @@ from jupyterhub.spawner import SimpleLocalProcessSpawner
 
 
 c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
+
 c.Spawner.args = ['--NotebookApp.allow_origin=*']
+c.JupyterHub.redirect_to_server = False
+c.JupyterHub.allow_named_servers = True
 c.JupyterHub.tornado_settings = {
             'headers': {
                 'Access-Control-Allow-Origin': "*",
@@ -33,5 +36,5 @@ c.ConfigurableHTTPProxy.extra_routes = {
     "/jupyterhub/sso/":"http://127.0.0.1:3000/jupyterhub/sso"
 }
 c.JupyterHub.base_url = "/jupyterhub"
-#c.JupyterHub.ssl_key = '/mnt/hdd3/tan/docs/k8s/jupyter-hub/JupyterHub-on-Kubernetes/images/jupyter-hub/jupyterhub/key.key'
-#c.JupyterHub.ssl_cert = '/mnt/hdd3/tan/docs/k8s/jupyter-hub/JupyterHub-on-Kubernetes/images/jupyter-hub/jupyterhub/key.crt'
+c.JupyterHub.ssl_key = '/mnt/hdd3/tan/code/jupyterhub/key.key'
+c.JupyterHub.ssl_cert = '/mnt/hdd3/tan/code/jupyterhub/key.crt'
